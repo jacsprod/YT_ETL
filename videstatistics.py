95d7b30e4860ac2d62884a8d2e9f60e33f4b9f68
Jorge Cruz Sandoval <jacs1111.prod@gmail.com>
2026-05-20 21:52:21 -0600

    videstatistics.py modificado

--- videstatistics.py
+++ videstatistics.py
@@ -10,6 +10,8 @@ API_KEY=os.getenv("API_KEY")
 
 CHANNEL_HANDLE="MrBeast"
 
+max_result=50
+
 def get_playlist_id():
     
     try:
@@ -40,12 +42,58 @@ def get_playlist_id():
     except  requests.exceptions.RequestException as e:
         
         print("Error",e)
+        
+        playlistid=get_playlist_id() 
+
+
+
+def get_video_ids(playlistid):
     
+    video_ids=[]    
+    pageToken=None
+     
+    base_url=f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_result}&playlistId={playlistid}&key={API_KEY}"  
+
+    try:
+        
+        while True:
+            
+            url = base_url
+            
+            if pageToken:
+                
+                url+=f"&pageToken={pageToken}"
+                
+            response = requests.get(url)    
+            
+            response.raise_for_status()
+            
+            data=response.json()
+            
+            for item in data.get('items',[]):
+                video_id = item['contentDetails']['videoId'] 
+                
+                video_ids.append(video_id)   
+                
+            pageToken =data.get("nextPageToken")
+            
+            if not pageToken:
+                break    
+            
+        return video_ids    
+        
+    except requests.exceptions.RequestException as e:
+        raise    
+
+        
 if  __name__ =="__main__":   
 
-    get_playlist_id()
+      playlistid=get_playlist_id()
     
-    print("get_playlist_id will be executed")
+      print(get_video_ids(playlistid))
     
-else:
-    print("get_playlist_id won be executed ")   
+    
+   # print("get_playlist_id will be executed")
+    
+#else:
+    #print("get_playlist_id won be executed ")   
